d0030a956408c0e213ef26289a7390075be18397
YUNJAEKIMPR <yunjae2094@gmail.com>
2022-12-30 11:46:20 +0700

    implement add/update/search category

--- order-management-system/src/kratos/oms/service/CategoryService.java
+++ order-management-system/src/kratos/oms/service/CategoryService.java
@@ -1,12 +1,16 @@
 package kratos.oms.service;
 
 import kratos.oms.domain.Category;
+import kratos.oms.model.category.CreateCategoryModel;
 import kratos.oms.model.category.SearchCategoryModel;
 import kratos.oms.repository.CategoryRepository;
+import kratos.oms.seedwork.Helpers;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class CategoryService {
     private final CategoryRepository categoryRepository;
@@ -16,10 +20,24 @@ public class CategoryService {
     }
 
     public List<Category> search(SearchCategoryModel searchModel) {
-        return categoryRepository.listAll();
+        List<Category> categories = categoryRepository.listAll();
+        Stream<Category> categoryStream = categories.stream();
+        if (!Helpers.isNullOrEmpty(searchModel.getName()))
+            categoryStream = categoryStream.filter(c -> c.getName().toUpperCase().contains(searchModel.getName().toUpperCase()));
+        return categoryStream.collect(Collectors.toList());
     }
 
     public Optional<Category> getDetail(UUID id) {
         return categoryRepository.listAll().stream().findFirst();
+    }
+
+    public Category add(CreateCategoryModel model) {
+        Category newCategory = new Category(model.getName());
+        categoryRepository.add(newCategory);
+        return newCategory;
+    }
+
+    public boolean delete(UUID id) {
+        return categoryRepository.delete(id);
     }
 }
